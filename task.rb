5258163c8df6d9a585109a2280c350aab675ec25
tomoya-kawabe <kawatomo238312@gmail.com>
2021-04-06 22:48:57 +0900

    Q19~Q20を提出します

--- task.rb
+++ task.rb
@@ -235,7 +235,11 @@ end
 class Item
   # 以下を修正して下さい
 
-  def initialize(name)
+  #変更点：attr_reader（ゲッター）の追記
+  attr_reader :name
+
+  #変更点：引数をキーワード引数に変更
+  def initialize(name:)
     @name = name
   end
 end
@@ -248,12 +252,31 @@ end
 
 class UserQ20
   # 以下に回答を記載
-
+  attr_reader :name, :age
+  def initialize(name:, age:)
+    @name = name
+    @age = age
+  end
 end
 
 class Zoo
   # 以下に回答を記載
+  def initialize(name:, entry_fee:)
+    @entry_fee = entry_fee
+  end
 
+  def info_entry_fee(user)
+    case user.age
+    when 0..5
+      puts "#{user.name}さんの入場料金は #{@entry_fee[:infant]} 円です。"
+    when 6..12
+      puts "#{user.name}さんの入場料金は #{@entry_fee[:children]} 円です。"
+    when 13..64
+      puts "#{user.name}さんの入場料金は #{@entry_fee[:adult]} 円です。"
+    when 65..120
+      puts "#{user.name}さんの入場料金は #{@entry_fee[:senior]} 円です。"
+    end
+  end
 end
 
 
